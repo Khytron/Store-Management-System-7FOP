200f63fcc0010ffeef51e0c38ac0538b7481b82e
Wing <yeewing286@gmail.com>
2026-01-04 23:25:45 +0800

    Add Search Sales Information

---
+++ src/service/SalesInfo.java
@@ -0,0 +1,71 @@
+package service;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import util.FilePath;
+
+public class SalesInfo {
+    private String keyword;
+    private List<String[]> SalesData = new ArrayList<>();
+
+    public SalesInfo(String keyword) {
+        this.keyword = keyword;
+    }
+
+    public boolean findSales(){
+        SalesData.clear();
+        boolean isFound=false;
+        try(BufferedReader br = new BufferedReader(new FileReader(FilePath.salesDataPath))) {
+            String firstLine = br.readLine(); // Skip header
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] fields = line.split(",");
+                for (String field : fields) {
+                    if (field.toLowerCase().contains(this.keyword.toLowerCase())) {
+                        SalesData.add(fields);
+                        isFound = true;
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return isFound;
+    }
+
+    public String getEmployeeName(String employeeID){
+        try(BufferedReader br = new BufferedReader(new FileReader(FilePath.employeeDataPath))) {
+            String firstLine = br.readLine(); // Skip header
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] row = line.split(",");
+                if (row[0].equals(employeeID)) {
+                    return row[1]; // Return employee name
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "Unknown Employee";
+    }
+
+    public void dislpaySalesInfo(){
+        for(String[] salesData : SalesData){
+            System.out.println("\nSales Record Found:");
+        
+            System.out.println("Date: " + salesData[7]);
+            System.out.println("Time: " + salesData[8]);
+            System.out.println("Customer: " + salesData[4]);
+            System.out.println("Item(s): " + salesData[2]);
+            System.out.println("Quantity: " + salesData[3]);
+            System.out.println("Total: RM" + salesData[6]);
+            System.out.println("Transaction Method: " + salesData[5]);
+            System.out.println("Employee: " + getEmployeeName(salesData[1]));
+            System.out.println("Status: Transaction Verified.");
+        }
+        
+    }
+}
